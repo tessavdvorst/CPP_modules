071d392eb38b88b996eb2dfee7a8e39016c0b4a6
Tessa Van der vorst <tessavdvorst@hotmail.com>
2023-02-07 15:46:00 +0100

    deleted comments in mod7 ex02

--- Module_07/ex02/Array.hpp
+++ Module_07/ex02/Array.hpp
@@ -6,22 +6,9 @@
 /*   By: Tessa <Tessa@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/05 16:52:40 by Tessa         #+#    #+#                 */
-/*   Updated: 2023/02/07 15:37:16 by tvan-der      ########   odam.nl         */
+/*   Updated: 2023/02/07 15:45:21 by tvan-der      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
-
-// Construction with no parameter: Creates an empty array.
-// • Construction with an unsigned int n as a parameter: Creates an array of n elements
-// initialized by default.
-// Tip: Try to compile int * a = new int(); then display *a.
-// • Construction by copy and assignment operator. In both cases, modifying either the
-// original array or its copy after copying musn’t affect the other array.
-// • You MUST use the operator new[] to allocate memory. Preventive allocation (allocating memory in advance) is forbidden. Your program must never access nonallocated memory.
-// • Elements can be accessed through the subscript operator: [ ].
-// • When accessing an element with the [ ] operator, if its index is out of bounds, an
-// std::exception is thrown.
-// • A member function size() that returns the number of elements in the array. This
-// member function takes no parameter and musn’t modify the current instance.
 
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
@@ -80,6 +67,5 @@ std::ostream& operator<<(std::ostream& output, const Array<T>& arr)
         output << arr[i] << "\n";
     return (output);
 }
-
 
 #endif
