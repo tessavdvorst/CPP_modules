3eda4489c72eab9cc59a99ff813e547281f37c8f
Tessa Van der vorst <tessavdvorst@hotmail.com>
2023-02-09 16:48:21 +0100

    finished mod8 ex00 however there is a weird bug with the return value of easyfind

---
+++ Module_08/ex00/easyfind.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   easyfind.hpp                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: tvan-der <tvan-der@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/02/08 11:02:48 by tvan-der      #+#    #+#                 */
+/*   Updated: 2023/02/09 16:42:53 by tvan-der      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+#include <iostream>
+#include <algorithm>
+
+template<typename T>
+typename T::iterator easyfind(T container, const int num) {
+	typename T::iterator iter = std::find(container.begin(), container.end(), num);
+	if (iter == container.end())
+		throw std::exception();
+	return (iter);
+}
+
+#endif
